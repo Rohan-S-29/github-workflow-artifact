5bb644fa88184b20888404c4b47261c6740d8a17
Rohan-S-29 <rohansarabu@gmail.com>
2025-03-06 16:18:11 +0530

    changed file structure

--- tests/test_pytest1.py
+++ tests/test_pytest1.py
@@ -1,14 +1,13 @@
-import math
-import pytest
+from app import add, subtract, multiply, divide
 
 def test_add():
-    assert math.add(2, 3) == 5
+    assert add(2, 3) == 5
 
 def test_subtract():
-    assert math.subtract(10, 5) == 5
+    assert subtract(10, 5) == 5
 
 def test_multiply():
-    assert math.multiply(4, 5) == 20
+    assert multiply(4, 5) == 20
 
 def test_divide():
-    assert math.divide(10, 2) == 5
+    assert divide(10, 2) == 5
--- tests/test_unittest1.py
+++ tests/test_unittest1.py
@@ -1,19 +1,19 @@
 import unittest
-import math
+import app
 
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(math.add(2, 3), 5)
+        self.assertEqual(app.add(2, 3), 5)
 
     def test_subtract(self):
-        self.assertEqual(math.subtract(10, 5), 5)
+        self.assertEqual(app.subtract(10, 5), 5)
 
     def test_multiply(self):
-        self.assertEqual(math.multiply(4, 5), 20)
+        self.assertEqual(app.multiply(4, 5), 20)
 
     def test_divide(self):
-        self.assertEqual(math.divide(10, 2), 5)
+        self.assertEqual(app.divide(10, 2), 5)
 
 if __name__ == "__main__":
     unittest.main()
